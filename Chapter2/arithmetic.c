cd14826d3d3351f3ed5216d94795c23f85c4d02a
rakibislam2233 <rakib2020.tkg@gmail.com>
2025-02-21 19:59:24 +0600

    ADD

--- Chapter2/arithmetic.c
+++ Chapter2/arithmetic.c
@@ -4,9 +4,10 @@
 int main()
 {
     int num1, num2;
+    printf("Enter two number : ");
     scanf("%d  %d", &num1, &num2);
     int jogfol = num1 + num2;
-    printf("%d + %d = %d \n", num1, num2, jogfol);
+    printf(" Total Jog fol :  %d + %d = %d \n", num1, num2, jogfol);
     int biyogfol = num1 - num2;
     printf("%d - %d = %d \n ", num1, num2, biyogfol);
     int gunfol = num1 * num2;
@@ -17,7 +18,17 @@ int main()
     // only integer ar jonno vagses ber korbo. karon float or dobule ar jonno . amader k real value tai dei. integer hole vag korle only vagfol dei but vagses dei na
 
     int vagses = num1 % num2;
-    printf("%d %% %d = %d", num1, num2, vagses);
+    printf("%d %% %d = %d\n", num1, num2, vagses);
+
+    int price = 10;
+    price += 10;
+    printf("Price Increment : %d\n", price);
+    price -= 5;
+    printf("Price Decrement : %d\n", price);
+    price++;
+    printf("Price ++ %d\n", price);
+    price--;
+    printf("Price -- %d\n", price);
 
     return 0;
 }
---
+++ Chapter2/arithmetic.problem.c
@@ -0,0 +1,12 @@
+// akta value ar borgo ber kora
+
+//pow only double ar jonno kaj kore
+#include <stdio.h>
+
+int main (){
+    int num1;
+    printf("Enter Your Number");
+    scanf("%d", &num1);
+    printf("Squre : %d ", pow(num1,2));
+    return 0;
+}
